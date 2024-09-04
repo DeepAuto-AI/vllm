4ebc53684a2099f985a3fa6e95536ed613f4f7cd
Heejun Lee <gmlwns5176@gmail.com>
2024-09-04 13:04:10 +0000

    update

--- vllm/model_executor/model_loader/__init__.py
+++ vllm/model_executor/model_loader/__init__.py
@@ -11,12 +11,16 @@ from vllm.model_executor.model_loader.utils import (
     get_architecture_class_name, get_model_architecture)
 
 
-def get_model(*, model_config: ModelConfig, load_config: LoadConfig,
-              device_config: DeviceConfig, parallel_config: ParallelConfig,
-              scheduler_config: SchedulerConfig,
-              lora_config: Optional[LoRAConfig],
-              multimodal_config: Optional[MultiModalConfig],
-              cache_config: CacheConfig) -> nn.Module:
+def get_model(*, 
+    model_config: ModelConfig, 
+    load_config: LoadConfig,
+    device_config: DeviceConfig, 
+    parallel_config: ParallelConfig,
+    scheduler_config: SchedulerConfig,
+    lora_config: Optional[LoRAConfig],
+    multimodal_config: Optional[MultiModalConfig],
+    cache_config: CacheConfig
+) -> nn.Module:
     loader = get_model_loader(load_config)
     return loader.load_model(model_config=model_config,
                              device_config=device_config,
